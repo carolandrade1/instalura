28bdb435a1d62cbde1fa72bdaa4bab53ee2225f0
Carol <carolandrades11@gmail.com>
2021-07-29 18:43:14 -0300

    Aula 05 - Fixing crossOrigin

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="https://i.postimg.cc/pVmd1F18/alura-Iconalura-Icon.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet" />
         
         <title>Instalura</title>
--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -8,21 +8,21 @@ export const GlobalStyle = createGlobalStyle`
 
   ${normalize}
 
-  body {
+  html, body {
     margin: 0;
     padding: 0;
     font-family: ${({ theme }) => theme.fontFamily};
   }
 
   /* Full height layout */
-    html, body {
-        display: flex;
-        min-height: 100vh;
-        width: 100%;
-    }
-    #__next {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-    }
+  html, body {
+    display: flex;
+    min-height: 100vh;
+    width: 100%;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
 `;
