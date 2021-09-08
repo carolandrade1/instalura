875fdd5a0ac874cfa3ca164e59c22742f9eec2d3
Carol <carolandrades11@gmail.com>
2021-09-08 09:58:24 -0300

    refactor: criando hook useForm no FormLogin

--- src/components/patterns/FormLogin/index.js
+++ src/components/patterns/FormLogin/index.js
@@ -1,18 +1,57 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import Button from '../../commons/Button';
 import TextField from '../../forms/TextField';
 
+function useForm({ initialValues, onSubmit }) {
+  const [values, setValues] = React.useState(initialValues);
+
+  return {
+    values,
+    handleSubmit(event) {
+      event.preventDefault();
+      onSubmit(values);
+    },
+    handleChange(event) {
+      const fieldName = event.target.getAttribute('name');
+      const { value } = event.target;
+
+      setValues((currentValues) => ({
+        ...currentValues,
+        [fieldName]: value,
+      }));
+    },
+  };
+}
+
 function LoginForm() {
+  const router = useRouter();
+  const initialValues = {
+    usuario: '',
+    senha: '',
+  };
+
+  const form = useForm({
+    initialValues,
+    onSubmit: (values) => {
+      router.push('/app/profile');
+    },
+  });
+
   return (
-    <form id="formCadastro" action="/app/profile">
+    <form id="formCadastro" action="/app/profile" onSubmit={form.handleSubmit}>
       <TextField
         placeholder="Usuário"
         name="usuario"
+        value={form.values.usuario}
+        onChange={form.handleChange}
       />
       <TextField
         placeholder="Senha"
         name="senha"
         type="password"
+        value={form.values.senha}
+        onChange={form.handleChange}
       />
 
       <Button
