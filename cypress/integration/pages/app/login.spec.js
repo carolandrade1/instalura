88801c16e091303bf82cba58fcb169e0b029fbeb
Carol <carolandrades11@gmail.com>
2021-09-08 16:29:52 -0300

    refactor: melhorando descrição do teste de login

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -2,29 +2,30 @@
 import LoginScreenPageObject from '../../../../src/components/screens/app/LoginScreen/LoginScreen.pageObject';
 
 describe('/pages/app/login/', () => {
-  // it === test que estamos fazendo
-  it('preencha os campos e vá para a página /app/profile', () => {
-    cy.intercept('https://instalura-api-git-master-omariosouto.vercel.app/')
-      .as('userLogin');
+  describe('when filling and submitting a form login request', () => {
+    it('go to the profile page', () => {
+      // Pré teste
+      cy.intercept('https://instalura-api-git-master-omariosouto.vercel.app/api/login')
+        .as('userLogin');
 
-    const loginScreen = new LoginScreenPageObject(cy);
+      // Cenário
+      const loginScreen = new LoginScreenPageObject(cy);
 
-    loginScreen
-      .fillLoginForm({ user: 'omariosouto', password: 'senhasegura' })
-      .submitLoginForm();
+      loginScreen
+        .fillLoginForm({ user: 'omariosouto', password: 'senhasegura' })
+        .submitLoginForm();
 
-    // o que esperamos? ir para "/app/profile/"
-    cy.url().should('include', '/app/profile');
+      // Asserções
+      cy.url().should('include', '/app/profile');
 
-    // Temos o token?
-    cy.wait('@userLogin')
-      .then((intercept) => {
-        // token do servidor
-        const { token } = intercept.response.body.data;
+      cy.wait('@userLogin')
+        .then((intercept) => {
+          const { token } = intercept.response.body.data;
 
-        cy.getCookie('APP_TOKEN').should('exist')
-          // token do cookie é igual ao do server?
-          .should('have.property', 'value', token);
-      });
+          cy.getCookie('APP_TOKEN').should('exist');
+          cy.getCookie('APP_TOKEN').should('exist')
+            .should('have.property', 'value', token);
+        });
+    });
   });
 });
