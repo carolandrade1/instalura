8169844b36711908dc6e35860705dea335f77b16
Carol <carolandrades11@gmail.com>
2021-09-08 16:22:04 -0300

    refactor: criando isStagingEnv

---
+++ src/infra/env/isStagingEnv.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+const isServer = typeof window === 'undefined';
+
+export const isStagingEnv = isServer
+  ? process.env.NODE_ENV === 'development'
+  : globalThis.location.href.includes('localhost');
