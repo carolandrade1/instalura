306aae06ce08ad483c487d3cd6f8e54263bcfb2a
Carol <carolandrades11@gmail.com>
2021-09-08 16:24:29 -0300

    refactor: definir BASE_URL baseado no isStagingEnv

--- src/services/login/loginService.js
+++ src/services/login/loginService.js
@@ -1,5 +1,7 @@
 import { setCookie, destroyCookie } from 'nookies';
 
+import { isStagingEnv } from '../../infra/env/isStagingEnv';
+
 /* eslint-disable import/prefer-default-export */
 async function HttpClient(url, { headers, body, ...options }) {
   return fetch(url, {
@@ -19,9 +21,13 @@ async function HttpClient(url, { headers, body, ...options }) {
     });
 }
 
+const BASE_URL = isStagingEnv
+  ? 'https://instalura-api-git-master-omariosouto.vercel.app'
+  : 'https://instalura-api.omariosouto.vercel.app';
+
 export const loginService = {
   async login({ username, password }) {
-    return HttpClient('https://instalura-api-git-master-omariosouto.vercel.app/api/login', {
+    return HttpClient(`${BASE_URL}/api/login`, {
       method: 'POST',
       body: {
         username,
