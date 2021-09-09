743964ace575c911a49f1b8814b60502a24661b0
Carol <carolandrades11@gmail.com>
2021-09-09 17:09:08 -0300

    refactor: refatorando teste de redirects usando snapshot

--- config/redirects.test.js
+++ config/redirects.test.js
@@ -2,13 +2,6 @@ import redirects from './redirects';
 
 describe('config/redirects', () => {
   test('renders all current redirects', () => {
-    const redirectsAtuais = [
-      {
-        source: '/login/',
-        destination: '/app/login/',
-        permanent: true,
-      },
-    ];
-    expect(redirects).toEqual(redirectsAtuais);
+    expect(redirects).toMatchSnapshot();
   });
 });
