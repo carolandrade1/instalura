3c3fd02e2f218d38675480201b75307e7cd2c364
Carol <carolandrades11@gmail.com>
2021-09-12 18:32:32 -0300

    refactor: testando quando o TextField é inválido e instalando lib @testing-library/user-event

--- src/components/forms/TextField/TextField.test.js
+++ src/components/forms/TextField/TextField.test.js
@@ -5,9 +5,44 @@ import TextField from './index';
 
 describe('<TextField />', () => {
   test('renders component', () => {
-    render(<TextField placeholder="Nome" value="ju" onChange={() => { }} name="nome" />);
+    render(
+      <TextField
+        placeholder="Nome"
+        value="Carol"
+        onChange={() => {}}
+        name="nome"
+      />,
+    );
 
+    // screen.debug();
     const textField = screen.getByPlaceholderText(/nome/i);
     expect(textField).toMatchSnapshot();
+  });
+
+
+  describe('when field is valid', () => {
+
+  });
+
+  describe('when field is invalid', () => {
+    // elemento tenha o span de texto
+    // muda o CSS da borda
+    test('displays the respective error message', () => {
+      render(
+        <TextField
+          placeholder="Email"
+          value="carol@gmail.com"
+          onChange={() => { }}
+          name="email"
+          isTouched
+          error="O campo email é obrigatório"
+        />,
+      );
+      const inputEmail = screen.getByPlaceholderText(/email/i);
+      expect(inputEmail).toHaveValue('carol@gmail.com');
+      expect(screen.getByRole('alert')).toHaveTextContent('O campo email é obrigatório');
+      expect(inputEmail).toMatchSnapshot();
+      // expect(true).toBe(true);
+    });
   });
 });
