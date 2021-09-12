db8663dd5f7c26fb3dfeb4c2a79023743573910b
Carol <carolandrades11@gmail.com>
2021-09-12 18:37:06 -0300

    refactor: testando quando TextField é válido

--- src/components/forms/TextField/TextField.test.js
+++ src/components/forms/TextField/TextField.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import user from '@testing-library/user-event';
 import { render, screen } from '../../../infra/test/testUtils';
 
 import TextField from './index';
@@ -19,9 +20,26 @@ describe('<TextField />', () => {
     expect(textField).toMatchSnapshot();
   });
 
-
   describe('when field is valid', () => {
+    describe('and user is typing', () => {
+      test('the value must be updated', () => {
+        const onChangeMock = jest.fn();
+        render(
+          <TextField
+            placeholder="Nome"
+            value=""
+            onChange={onChangeMock}
+            name="nome"
+            isTouched
+          />
+          ,
+        );
 
+        const inputNome = screen.getByPlaceholderText(/nome/i);
+        user.type(inputNome, 'devsoutinho@gmail.com');
+        expect(onChangeMock).toHaveBeenCalledTimes(21);
+      });
+    });
   });
 
   describe('when field is invalid', () => {
