59afd349ef88edb86aa0f7f0687c3947986aab46
Carol Andrade Santos <carolandrades11@gmail.com>
2021-09-21 10:04:11 -0300

    feat: criando estilo pagina Sobre

--- pages/sobre/index.js
+++ pages/sobre/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
-import Box from '../../src/components/foundation/Layout/Box';
+import AboutScreen from '../../src/components/screens/AboutScreen';
 import websitePageHOC from '../../src/components/wrappers/WebsitePage/hoc';
-
-const Title = styled.h1`
-  text-align: center;
-`;
 
 function AboutPage() {
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      flex="1"
-    >
-      <Title>Sobre</Title>
-    </Box>
+    <AboutScreen />
   );
 }
 
