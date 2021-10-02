f4befc4c4ee0f178f822092fbcb23c42482b64bb
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-10-02 17:35:48 -0300

    refatorar websitePageHOC para aceitar contextValues

--- src/components/wrappers/WebsitePage/hoc/index.js
+++ src/components/wrappers/WebsitePage/hoc/index.js
@@ -9,15 +9,22 @@ export default function websitePageHOC(
   PageComponent,
   { pageWrapperProps } = { pageWrapperProps: {} },
 ) {
-  return (props) => (
-    <WebsiteGlobalProvider>
-      <WebsitePageWrapper
-        {...pageWrapperProps}
-        {...props.pageWrapperProps}
-        messages={props.messages}
-      >
-        <PageComponent {...props} />
-      </WebsitePageWrapper>
-    </WebsiteGlobalProvider>
-  );
+  return (props) => {
+    // passa pro PageComponent as props que não sejam
+    // contextValues ou pageWrapperProps
+    // OBS: a renomeção para pageWrapperPropsFromProps foi só pra
+    // evitar o conflito da variavel ja definida na linha 10
+    const { contextValues, pageWrapperProps: pageWrapperPropsFromProps, ...rest } = props;
+    return (
+      <WebsiteGlobalProvider>
+        <WebsitePageWrapper
+          {...pageWrapperProps}
+          {...pageWrapperPropsFromProps}
+          contextValues={contextValues}
+        >
+          <PageComponent {...rest} />
+        </WebsitePageWrapper>
+      </WebsiteGlobalProvider>
+    );
+  };
 }
